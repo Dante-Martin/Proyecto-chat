e0aee6e7ae0fdb1e5e93cbc6b6eb21d8f6ca8d0f
dante <Dante_Martin@outlook.com>
2023-01-31 18:16:37 -0300

    agregado de MongoDB

--- store.js
+++ store.js
@@ -2,18 +2,30 @@
 
 // const { response } = require("express");
 
-const list = []; //guardamos los mensajes
+const db = require("mongoose"); //const list = []; guardamos los mensajes
+
+db.Promise = global.Promise;
+
+db.connect("mongodb+srv://portafolio.tjophe6.mongodb.net/test?authMechanism=DEFAULT", { userNewParser: true });
+
+console.log("[db] conectado con exito");
 
 function addMessage(usermsg) {
   //recibira el mensaje
-  list.push(usermsg);
-  console.log("Elementos de la lista:", list);
+  // list.push(usermsg);
+  const myMessage = new model(usermsg);
+  myMessage.save();
+  // console.log("Elementos de la lista:", list);
 }
 
-function getMessages() {
-  console.log("funca getMessages store");
-  return list;
+async function getMessages() {
+  const usermsg = await model.find();
+  return usermsg;
 }
+// function getMessages() {
+//   console.log("funca getMessages store");
+//   return list;
+// }
 module.exports = {
   add: addMessage,
   list: getMessages,
